662fb359ebdf045afbec9e5849c87fe7ee552a7a
Himanshu <hsperfect1111@gmail.com>
2025-08-07 23:31:30 +0530

    Permutations

--- Recursion/011_permutations.cpp
+++ Recursion/011_permutations.cpp
@@ -42,3 +42,51 @@ public:
 
 // 1 2 3 4 5
 // return all the possible permutations = 5*4*3*2*1 = 5! = 120
+
+// Doubt Session - 10
+// 12-Nov-2024 (Tue)
+
+// METHOD : 2 is easy and understood
+
+// Watch the video for better understanding 
+
+class Solution {
+public:
+    // We create a final answer vector to store all the combinations to be returned
+    vector<vector<int>> answer = {};
+
+    // R(0) ->  0 -- 0,   0 -- 1 ,    0 -- 2
+              // index  i    index  i+1    index  i+2
+
+    void recursion(int index, vector<int>& nums)
+    {
+        // Base case ~ STOP condition
+        if(index == nums.size())
+        {
+            answer.push_back(nums);
+            return;
+        }
+
+        // Recursive case ~ options
+        for(int i = index; i < nums.size(); i++)
+        {
+            // Choose the i-th option - swap -> nums[i], nums[index]
+            swap(nums[i], nums[index]);
+
+            recursion(index + 1, nums);
+
+            // Undo the state (backtrack)
+            swap(nums[i], nums[index]);
+        }
+    }
+
+    vector<vector<int>> permute(vector<int>& nums) {
+
+        recursion(0, nums);
+
+        return answer;
+    }
+};
+
+// In recursion, if there are 'n' options, we use a for-loop inside recursion
+// And its Time Complexity = n^n
